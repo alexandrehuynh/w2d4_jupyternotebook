23c5a6f0ec879332766fd00a8085534cbff76044
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-01-11 13:48:14 -0800

    first commit

---
+++ geometry.py
@@ -0,0 +1,13 @@
+# 1) Create a Module in VS Code and Import It into jupyter notebook
+
+# Module should have the following capabilities:
+from math import pi as p
+
+# 1a) Has a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+def house_sqft(length, width):
+    return (length * width)
+
+# 1b) Has a function to calculate the circumference of a circle 2 Pi r
+def circumference(radius):
+    return (2 *p *radius)
